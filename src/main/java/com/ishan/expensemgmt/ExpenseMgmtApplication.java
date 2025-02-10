e5a41b99bbf13317a8288c7521dafc70f8618641
Ansh Kumar <aanshkumar321@gmail.com>
2025-02-10 20:50:01 +0530

    Splitwie commit 1

---
+++ src/main/java/com/ishan/expensemgmt/ExpenseMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.ishan.expensemgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseMgmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseMgmtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ishan/expensemgmt/ExpenseMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishan.expensemgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseMgmtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
